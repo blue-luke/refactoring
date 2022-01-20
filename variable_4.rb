1f6f82c1f74a8f3b91adf840fd6d0f03c195dab5
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-20 12:50:29 +0000

    Done variable 4 exercise. Not sure that url and api_key should be treated the same way

--- variable_4.rb
+++ variable_4.rb
@@ -2,13 +2,17 @@ class Mail
   def initialize(email, fancy = false)
     @email = email
     @fancy = fancy
+    @url = "http://mixmax.com"
+    @api_key = "f20506xx808c"
+    @plain_greeting = "Welcome to MyProduct"
+    @fancy_greeting = "Welcome to MyProduct, fancy person!"
   end
 
   def send_message
     if fancy
-      MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct, fancy person!" })
+      MailServer.connect(@url, api_key: @api_key).send_message({ to: @email, body: "#{@fancy_greeting}" })
     else
-      MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct" })
+      MailServer.connect(@url, api_key: @api_key).send_message({ to: @email, body: "#{@plain_greeting}" })
     end
   end
 end
