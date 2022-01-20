6cb6d7086a7968244575d9de61cb3d45eadab559
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-20 12:55:27 +0000

    Method 1 exercise done, not 100% happy with it

--- method_1.rb
+++ method_1.rb
@@ -1,6 +1,10 @@
-def send_email_to(user)
+def create_email_to(user)
   email = user.email.strip
   mail  = Mail.new(email)
   
+  send_email(mail)
+end
+
+def send_email(mail)
   mail.send_message
 end
