06a4163c229d074136510f3c6a01070b23e26dee
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-21 11:35:46 +0000

    Not doing method 4 exercise, not good use of time

--- method_4.rb
+++ method_4.rb
@@ -8,8 +8,13 @@ class Mail
     mail_server = MailServer.connect("http://mixmax.com", api_key: "f20506xx808c")
     base_body = "Welcome to MyProduct"
 
-    body = @fancy ? "#{ base_body }, fancy person!" : base_body
+    
 
     mail_server.send_message({ to: @email, body: base_body })
   end
+
+  def message_body(fancy)
+    @fancy ? "#{ base_body }, fancy person!" : base_body
+  end
 end
+
